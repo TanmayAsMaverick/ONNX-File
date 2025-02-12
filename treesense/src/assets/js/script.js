95a8a0df33a42f74f4245ba0d8a98ace74f8fd98
TanmayPatil25 <tanmaypatil1729@gmail.com>
2025-02-12 16:45:48 +0530

    Added ONNX model and other files

---
+++ treesense/src/assets/js/script.js
@@ -0,0 +1,14 @@
+// JavaScript code to toggle mobile menu visibility
+// const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+// const mobileMenu = document.querySelector('.mobile-menu');
+
+// mobileMenuIcon.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('active');
+// });
+
+function toggleMenu() {
+    let menu = document.getElementById("mobileMenu");
+    menu.classList.toggle("hidden");
+  }
+  
+  
